63ae2ae10c88271c0b3b052a5dc4239ed2041f83
dhanush-danny <78251450+dhanush-danny@users.noreply.github.com>
2021-12-21 11:53:15 +0530

    Add files via upload

---
+++ calculator_app.py
@@ -0,0 +1,14 @@
+input_multi_1=int(input("Enter Number Here For Multiplication "))
+input_multi_2=int(input("Enter Number Here For Multiplication "))
+ans_multi=(input_multi_1*input_multi_2)
+print(ans_multi)
+
+input_addi_1=int(input("Enter Number Here For Addition "))
+input_addi_2=int(input("Enter Number Here For Addition "))
+ans_addi=(input_addi_1+input_addi_2)
+print(ans_addi)
+
+input_subtract_1=int(input("Enter Number Here For Subtraction "))
+input_subtract_2=int(input("Enter Number Here For Subtraction "))
+ans_sub=(input_subtract_1-input_subtract_2)
+print(ans_sub)
